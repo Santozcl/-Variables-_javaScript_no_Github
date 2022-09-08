7e30b25c0731939f21fa8aaa6f227e180c9e778c
Santozcl <carloseduardodossantossilva111@gmail.com>
2022-09-08 17:17:01 -0300

    files

---
+++ JS.js
@@ -0,0 +1,5 @@
+const prompt = require ('prompt-sync')()
+const peso = prompt (' qual seu peso?');
+const nome = prompt ('Qual seu nome? ');
+const altura = prompt  ('qual sua altura? ');
+const Internação = prompt ('voce é diabetico? ');
